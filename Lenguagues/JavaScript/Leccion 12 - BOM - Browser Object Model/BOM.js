05613cef0d4741059824ce63fe05c56272a2c4e1
Harol Daniel Naranjo Yunez <69650927+DaniielNaranjo@users.noreply.github.com>
2021-10-04 16:08:42 -0500

    Eventos y Carpeta de leccion 14 bloque de ejercicios

--- Lenguagues/JavaScript/Leccion 12 - BOM - Browser Object Model/BOM.js
+++ Lenguagues/JavaScript/Leccion 12 - BOM - Browser Object Model/BOM.js
@@ -1,2 +1,25 @@
 'use strict'
 
+/*
+ 		BOM - Browser Object Model
+ 	window. - acceder a propiedades de la pestaña.
+ 	window.innerHeight(Altura), window.innerWidth(Anchura) - obtener tamaño de la pestaña.
+ 	screen.height, screen.width - obtiene el tamaño de la pestaña al momento de entrar en ella.
+ 	window.location - obtiene el objeto localizacion de la pagina(URL).
+	window.open - abre una nueva pestaña.
+*/
+
+function getBOM(){
+	console.log(screen.height);
+	console.log(screen.width);
+	console.log(window.location);
+}
+
+function redirect(url){
+	window.location.href = url;
+}
+
+function abrirVentana(url) {
+	//Agregar parametros deseados 
+	window.open(url, " " ,"width=400, height=300");
+}
